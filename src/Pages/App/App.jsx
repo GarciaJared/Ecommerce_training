a14110e8296209ae5e4fe6a94b833425e33417d2
yeyeJared <gjared388@gmail.com>
2023-07-12 09:05:23 -0600

    add react router dom

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,27 +1,32 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
-import MyAccount from "../MyAccount/MyAccount"
-import MyOrder from '../MyOrder/MyOrder';
-import MyOrders from '../MyOrders/MyOrders';
+import MyAccount from "../MyAccount/MyAccount";
+import MyOrder from "../MyOrder/MyOrder";
+import MyOrders from "../MyOrders/MyOrders";
 // import { NotFound } from "../NotFound/NotFound";
-// import { SignIn } from "../Signin/SignIn";
+import SignIn from "../Signin/SignIn";
 import "./App.css";
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/", element: <MyAccount /> },
+    { path: "/MyOrder", element: <MyOrder /> },
+    { path: "/MyOrders", element: <MyOrders /> },
+    { path: "/SignIn", element: <SignIn /> },
+    //{path: "/*",element: <NotFound />}
+  ]); //obj to save routes
+  return routes;
+};
+
+const App = () => {
   return (
     <>
-      <div className="bg-red-800	">
-        <Home></Home>
-
-        <MyAccount></MyAccount>
-
-        <MyOrder></MyOrder>
-
-        <MyOrders></MyOrders>
-
-        <Home></Home>
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
