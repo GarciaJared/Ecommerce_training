02ee9576409024ab12892d0c9b72a4f60b4fb6cc
yeyeJared <gjared388@gmail.com>
2023-07-12 13:19:28 -0600

    Add components folder and navbar prev

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  let activeStyle = 'underline underline-offset-4'; 
+  
+  return (
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+      <ul className="flex items-center gap-3">
+        <li className="font-semibold text-lg">
+          <NavLink to="/">ShopU</NavLink>
+        </li>
+        <li>@GarciaJared</li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/myOrder"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            My orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/MyOrders"  className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >My orders</NavLink>
+        </li>
+        <li>
+          <NavLink to="/MyAccount"  className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >My account</NavLink>
+        </li>
+        <li>
+          <NavLink to="/SignIn"  className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >SignIn</NavLink>
+        </li>
+        <li>🛒0</li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -6,16 +6,17 @@ import MyOrders from "../MyOrders/MyOrders";
 // import { NotFound } from "../NotFound/NotFound";
 import SignIn from "../Signin/SignIn";
 import "./App.css";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/", element: <MyAccount /> },
+    { path: "/MyAccount", element: <MyAccount /> },
     { path: "/MyOrder", element: <MyOrder /> },
     { path: "/MyOrders", element: <MyOrders /> },
     { path: "/SignIn", element: <SignIn /> },
     //{path: "/*",element: <NotFound />}
-  ]); //obj to save routes
+  ]); //array to save routes
   return routes;
 };
 
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <AppRoutes />
       </BrowserRouter>
     </>
