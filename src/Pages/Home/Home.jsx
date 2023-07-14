589cc64c0f3591e477a827fae4ae607e97a19222
yeyeJared <gjared388@gmail.com>
2023-07-14 16:13:03 -0600

    Add FETCH API hooks and card component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,22 @@
 import "./home.css";
 import Layout from "../../Components/Layout/Layout";
+import Card from "../../Components/Card/Card";
+import useFetch from "../../Hooks/FetchHooks";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  const url = "https://fakestoreapi.com/products";
+  const { data: products } = useFetch(url, null);
+
+  return (
+    <Layout>
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {products?.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
